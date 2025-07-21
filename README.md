d578f9df6fa3973de4b3fd47496cf5b122258f57
Wei(Tom) Xie <tom_xie@sfu.ca>
2025-07-20 18:00:58 -0700

    Initial commit. Flux variability analyses on BG-11 media components in a Marimo notebook.

---
+++ README.md
@@ -0,0 +1,50 @@
+# UBC iGEM 2025 Metabolic Modelling (Python)
+
+Repository for all metabolic modelling of _Synechococcus elongatus_ UTEX 2973 and _Caulobacter vibrioides_ CB2A to aid/inform engineering efforts of said microbes.
+
+Currently, mainly [flux balance analysis (FBA)](https://doi.org/10.1038/nbt.1614) using the [COBRApy](https://cobrapy.readthedocs.io) package.
+
+## Data
+
+* UTEX 2973:
+    * genome-scale metabolic model from Mueller, T., Ungerer, J., Pakrasi, H. et al. Identifying the Metabolic Differences of a Fast-Growth Phenotype in Synechococcus UTEX 2973. Sci Rep 7, 41569 (2017). https://doi.org/10.1038/srep41569
+
+## Getting Started
+
+What are the primary analysis scripts/starting points? How would the analysis be rerun?
+
+```
+Give an example
+```
+
+### Requirements
+
+A step by step series of examples that tell you how to get a development/analysis env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using. It is also worth listing what the requirements are.
+
+## Contributing
+
+_Details regarding how to contribute. Coding style conventions, use of tests, etc._
+
+## Notes
+
+_Additional notes / precatuions / etc. to make users aware of._
+
+## License
+
+## Acknowledgments
+
+* README template taken [from Daniel E. Cook](https://gist.github.com/danielecook/94272f387d3366070d2546e2eadefe57).
---
+++ notebooks/cyano_growth_media.py
@@ -0,0 +1,283 @@
+import marimo
+
+__generated_with = "0.14.12"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Optimizing Media of _Synechococcus elongatus_ UTEX 2973 for Max Growth
+        This notebook aims to computationally predict the best variations of the media we are using for our cyanobacterium strain (_S. elongatus_ UTEX 2973), BG-11 media, for fastest growth.
+
+        Following standard flux balance analysis (FBA) theory, the **objective** here is formulated as **maximizing the *flux* of** a "fake" reaction formulated purely to track **biomass accumulation**, therein serving as a proxy for growth rate of the cells.
+
+        The parameters that we will vary and **search over** are the **maximum allowed uptake rates of each of the BG-11 media components**.
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## Load model_extended_constraints")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        We are using the
+        >composite GSM model for both Synechococcus 7942 and Synechococcus 2973
+
+        developed in (Mueller et. al., 2017).
+
+        The two models are in `SBMLmodel_UTEX2973.xml` and `SBMLmodel_PCC7942.xml` corresponding to supplementary files 2 and 3 of (Mueller et. al., 2017) respectively. The latter should be the one for UTEX 2973, as opposed to PCC 7942.
+
+        ---
+
+        Mueller, T., Ungerer, J., Pakrasi, H. et al. Identifying the Metabolic Differences of a Fast-Growth Phenotype in Synechococcus UTEX 2973. Sci Rep 7, 41569 (2017). https://doi.org/10.1038/srep41569
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"_Note_: The below cell might take a bit long to run if the SBML model is big.")
+    return
+
+
+@app.cell
+def _():
+    from pathlib import Path
+    import marimo as mo
+    import cobra
+    from cobra.io import read_sbml_model, save_json_model
+    from cobra import Model, Reaction, Metabolite
+
+    # RAW_DATA_DIR = Path(__file__).parent / "data" / "raw"
+    # PROCD_DATA_DIR = Path(__file__).parent / "data" / "processed"
+    ROOT_DIR = Path("/home/tom/Public/UBCiGEM/metabolic_modelling")
+    RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
+    PROCD_DATA_DIR = ROOT_DIR / "data" / "processed"
+
+    model = read_sbml_model(RAW_DATA_DIR / "SBMLmodel_UTEX2973.xml")
+    return mo, model
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Finding the Media Components in the Model
+        Well actually, finding the ID's of the exchange reactions in the model *corresponding to* the media components.
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    BG_11_MEDIA_EXRXNS_IDS = {
+        "EX_CO2",
+        "EX_PHO1",
+        "EX_PHO2",
+        "EX_Citrate",
+        "EX_NH3",
+        "EX_Mn2_",
+        "EX_Zinc",
+        "EX_Cu2_",
+        "EX_Molybdate",
+        "EX_Co2_",
+        "EX_Nitrate",
+        "EX_Phosphate",
+        "EX_Sulfate",
+        "EX_Fe3_",
+        "EX_H2CO3",
+        "EX_Calcium"
+    }
+    return (BG_11_MEDIA_EXRXNS_IDS,)
+
+
+@app.cell
+def _(BG_11_MEDIA_EXRXNS_IDS, model):
+    for _rxn_id in BG_11_MEDIA_EXRXNS_IDS:
+        if _rxn_id not in model.reactions:
+            raise ValueError(f'Reaction {_rxn_id} not found in the model.')
+        else:
+            print(model.reactions.get_by_id(_rxn_id))
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ### Oxygen
+        On Mars, no oxygen $\implies v_{\mathtt{EX\_O2}} \geq 0$.
+        """
+    )
+    return
+
+
+@app.cell
+def _(model):
+    print(f"Default bounds on oxygen exchange reaction: {model.reactions.EX_O2.lower_bound} < v_{{EX_O2}} < {model.reactions.EX_O2.upper_bound}")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## Flux Variability Analysis on BG-11 Components Uptakes")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        Extend the default flux constraints in the model so we actually explore enough of the space of possibilities.
+
+        From the above (_TODO_) phenotypic phase plane analyses, we know optimal...
+        - CO2 = -132 mmol/gDW/h, photosystem I = photosystem II = -900 mmol/gDW/h
+        - CO2 = -30 mmol/gDW/h, NH3 = -30 mmol/gDW/h
+
+        Let's go to...
+        - -2000 $\leq$ CO2
+        - -2500 $\leq$ photosystems
+        - -1000 $\leq$ others
+        """
+    )
+    return
+
+
+@app.cell
+def _(BG_11_MEDIA_EXRXNS_IDS, model):
+    model_extended_constraints = model.copy()
+    for _rxn_id in BG_11_MEDIA_EXRXNS_IDS:
+        _rxn = model_extended_constraints.reactions.get_by_id(_rxn_id)
+        if _rxn.lower_bound > -1500:
+            _rxn.lower_bound = -1500
+    model_extended_constraints.reactions.EX_CO2.lower_bound = -2000
+    model_extended_constraints.reactions.EX_PHO1.lower_bound = -2500
+    model_extended_constraints.reactions.EX_PHO2.lower_bound = -2500
+    for _rxn_id in BG_11_MEDIA_EXRXNS_IDS:
+        _rxn = model_extended_constraints.reactions.get_by_id(_rxn_id)
+        print(f'{_rxn.id}: {_rxn.lower_bound} <= flux <= {_rxn.upper_bound}')
+    return (model_extended_constraints,)
+
+
+@app.cell
+def _(BG_11_MEDIA_EXRXNS_IDS, model_extended_constraints):
+    from cobra.flux_analysis import flux_variability_analysis
+    fva_results = flux_variability_analysis(model_extended_constraints, [model_extended_constraints.reactions.get_by_id(_rxn_id) for _rxn_id in BG_11_MEDIA_EXRXNS_IDS], fraction_of_optimum=1, loopless=True)
+    fva_results
+    return flux_variability_analysis, fva_results
+
+
+@app.cell
+def _(fva_results):
+    from matplotlib import pyplot as plt
+    plt.figure(figsize=(6, 10))
+    _ax = plt.gca()
+    _bar_plot = plt.barh(fva_results.index, fva_results['maximum'] - fva_results['minimum'], left=fva_results['minimum'], color='violet')
+    for (_i, (_min_val, _max_val)) in enumerate(zip(fva_results['minimum'], fva_results['maximum'])):
+        _ax.plot(_min_val, _i, marker='|', color='black', markersize=18, markeredgewidth=2)
+        _ax.plot(_max_val, _i, marker='|', color='black', markersize=18, markeredgewidth=2)
+    _ax.set_title('Flux Variability Analysis of BG-11 Media Uptake Reactions')
+    _ax.set_xlabel('Flux Range (mmol/gDW/h)')
+    _ax.set_ylabel('Reactions')
+    plt.tight_layout()
+    plt.show()
+    return (plt,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ### _Note_: Beware Loops
+        The [COBRApy docs](https://cobrapy.readthedocs.io/en/latest/simulating.html#Running-FVA) note that unrealistic **loops** of reactions may be simulated, leading to some **unrealistically high fluxes**. Luckily, COBRApy includes detecting such loops and ensuring solutions without their resulting artifacts.
+
+        Let's see if we got any such loops here.
+        """
+    )
+    return
+
+
+@app.cell
+def _(BG_11_MEDIA_EXRXNS_IDS, flux_variability_analysis, model):
+    loopless_fva_results = flux_variability_analysis(model, [model.reactions.get_by_id(_rxn_id) for _rxn_id in BG_11_MEDIA_EXRXNS_IDS], fraction_of_optimum=1, loopless=True)
+    loopless_fva_results
+    return (loopless_fva_results,)
+
+
+@app.cell
+def _(fva_results, loopless_fva_results):
+    # check the difference between loopless and non-loopless FVA
+    fva_results - loopless_fva_results
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ### No Longer Using CO2 as Main Carbon Source?
+        Ideally, we would want cyano to use CO2 as citrate, carbonic acid, etc. would need to be transported to Mars.
+        """
+    )
+    return
+
+
+@app.cell
+def _(BG_11_MEDIA_EXRXNS_IDS, model):
+    model_extended_constraints_1 = model.copy()
+    EXTEND_EXRXNS_IDS = {'EX_CO2', 'EX_PHO1', 'EX_PHO2', 'EX_NH3', 'EX_Mn2_', 'EX_Zinc', 'EX_Cu2_', 'EX_Molybdate', 'EX_Co2_', 'EX_Nitrate', 'EX_Phosphate', 'EX_Sulfate', 'EX_Fe3_', 'EX_Calcium'}
+    for _rxn_id in EXTEND_EXRXNS_IDS:
+        _rxn = model_extended_constraints_1.reactions.get_by_id(_rxn_id)
+        if _rxn.lower_bound > -1500:
+            _rxn.lower_bound = -1500
+    model_extended_constraints_1.reactions.EX_CO2.lower_bound = -2000
+    model_extended_constraints_1.reactions.EX_PHO1.lower_bound = -2500
+    model_extended_constraints_1.reactions.EX_PHO2.lower_bound = -2500
+    model_extended_constraints_1.reactions.EX_Citrate.lower_bound = -1
+    model_extended_constraints_1.reactions.EX_H2CO3.lower_bound = -10
+    for _rxn_id in BG_11_MEDIA_EXRXNS_IDS:
+        _rxn = model_extended_constraints_1.reactions.get_by_id(_rxn_id)
+        print(f'{_rxn.id}: {_rxn.lower_bound} <= flux <= {_rxn.upper_bound}')
+    return (model_extended_constraints_1,)
+
+
+@app.cell
+def _(
+    BG_11_MEDIA_EXRXNS_IDS,
+    flux_variability_analysis,
+    model_extended_constraints_1,
+):
+    fva_results_1 = flux_variability_analysis(model_extended_constraints_1, [model_extended_constraints_1.reactions.get_by_id(_rxn_id) for _rxn_id in BG_11_MEDIA_EXRXNS_IDS], fraction_of_optimum=1, loopless=True)
+    fva_results_1
+    return (fva_results_1,)
+
+
+@app.cell
+def _(fva_results_1, plt):
+    plt.figure(figsize=(6, 10))
+    _ax = plt.gca()
+    _bar_plot = plt.barh(fva_results_1.index, fva_results_1['minimum'] - fva_results_1['maximum'], left=-fva_results_1['minimum'], color='violet')
+    for (_i, (_min_val, _max_val)) in enumerate(zip(-fva_results_1['maximum'], -fva_results_1['minimum'])):
+        _ax.plot(_min_val, _i, marker='|', color='black', markersize=18, markeredgewidth=2)
+        _ax.plot(_max_val, _i, marker='|', color='black', markersize=18, markeredgewidth=2)
+    _ax.set_title('Flux Variability Analysis of BG-11 Media Uptake Reactions')
+    _ax.set_xlabel('Uptake Range (mmol/gDW/h)')
+    _ax.set_ylabel('Reactions')
+    plt.tight_layout()
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
